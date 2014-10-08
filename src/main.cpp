7a09481a4ff03f2cf404a1c464f318790f8e9267
stratosphr <stratosphr@stratosphr.(none)>
2014-10-08 23:18:12 +0200

    Adding NotGate class.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "clockGate.h"
 #include "trueGate.h"
 #include "falseGate.h"
-#include "andGate.h"
+#include "notGate.h"
 #include "watch.h"
 #include <thread>
 
@@ -12,20 +12,19 @@ int main(int argc, char* argv[]){
     (void) argv;
 
     ClockGate clock1("clock1", true, 1);
-    ClockGate clock2("clock2", false, 1);
     FalseGate false1("false1");
     TrueGate true1("true1");
-    AndGate and1;
-    Watch andWatch(&and1);
+    NotGate not1;
 
-    clock1.addSuccessor(&and1);
-    clock2.addSuccessor(&and1);
+    Watch watch2(&clock1);
+    Watch watch1(&not1);
 
-    and1.join();
+    clock1.addSuccessor(&not1);
+
+    not1.join();
     clock1.join();
     false1.join();
     true1.join();
-    clock2.join();
 
     return 0;
 }
